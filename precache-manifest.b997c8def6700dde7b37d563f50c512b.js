19d522514776f821548ff331e1efb1b354b96142
chandrika <chandrikabiswal1@gmail.com>
2020-05-24 18:23:25 +0530

    Updates

---
+++ precache-manifest.b997c8def6700dde7b37d563f50c512b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37063c54184f9fd8244e",
+    "url": "/my-website/static/js/runtime~main.37063c54.js"
+  },
+  {
+    "revision": "c79aeddaeef17932ceba",
+    "url": "/my-website/static/js/main.c79aedda.chunk.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/my-website/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "c79aeddaeef17932ceba",
+    "url": "/my-website/static/css/main.c0ac1f0a.chunk.css"
+  },
+  {
+    "revision": "e91f50304881fff0fbee94b59d796231",
+    "url": "/my-website/index.html"
+  }
+];
